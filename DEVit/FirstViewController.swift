83c52c4395a9c4f7a819639e9b4ba0b21953bf8b
attheodo <at@atworks.gr>
2016-10-26 20:41:14 +0300

    initial commit

---
+++ DEVit/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DEVit
+//
+//  Created by Athanasios Theodoridis on 26/10/2016.
+//  Copyright © 2016 devitconf. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
