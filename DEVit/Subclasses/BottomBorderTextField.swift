82c8e7fe5de388ff0d72ee5309d248fbc671d1c8
attheodo <at@atworks.gr>
2016-10-28 14:14:41 +0300

    add subclasses

---
+++ DEVit/Subclasses/BottomBorderTextField.swift
@@ -0,0 +1,9 @@
+//
+//  BottomBorderTextField.swift
+//  DEVit
+//
+//  Created by Athanasios Theodoridis on 27/10/2016.
+//  Copyright © 2016 devitconf. All rights reserved.
+//
+
+import Foundation
---
+++ DEVit/Subclasses/CTAButton.swift
@@ -0,0 +1,9 @@
+//
+//  CTAButton.swift
+//  DEVit
+//
+//  Created by Athanasios Theodoridis on 27/10/2016.
+//  Copyright © 2016 devitconf. All rights reserved.
+//
+
+import Foundation
