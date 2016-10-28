964086bbcf2f92bb44e0f93d33bd8f95e85562e4
attheodo <at@atworks.gr>
2016-10-28 17:04:47 +0300

    configure page menu

--- DEVit/SessionsViewController.swift
+++ DEVit/SessionsViewController.swift
@@ -8,12 +8,52 @@
 
 import UIKit
 
-import PagingMenuController
+import PageMenu
 
 class SessionsViewController: UIViewController {
 
+    private var pageMenu: CAPSPageMenu!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        _configureView()
+        _configurePageMenu()
+
+    }
+    
+    // MARK: - Private Methods
+    private func _configureView() {
+        view.backgroundColor = Colors.lightBlue
+    }
+    private func _configurePageMenu() {
+        
+        let talksVC = TalksViewController.instantiateFromStoryboard()
+        talksVC.title = "Talks"
+        
+        let workshopsVC = WorkshopsViewController.instantiateFromStoryboard()
+        workshopsVC.title = "Workshops"
+        
+        let options:[CAPSPageMenuOption] = [
+            // Layout
+            .selectionIndicatorHeight(0),
+            .menuHeight(60),
+            .menuItemSeparatorWidth(0),
+            .menuItemWidthBasedOnTitleTextWidth(true),
+            // Look & feel
+            .viewBackgroundColor(Colors.lightBlue!),
+            .scrollMenuBackgroundColor(Colors.lightBlue!),
+            .selectedMenuItemLabelColor(Colors.darkestBlue!),
+            .unselectedMenuItemLabelColor(Colors.darkestBlueFaded!),
+            .menuItemFont(UIFont.systemFont(ofSize: 26.0, weight: UIFontWeightHeavy)),
+            .useMenuLikeSegmentedControl(false),
+        ]
+        
+        pageMenu = CAPSPageMenu(
+            viewControllers: [talksVC, workshopsVC],
+            frame: CGRect(x:0.0, y:22.0, width: self.view.frame.width, height: self.view.frame.height),
+            pageMenuOptions: options)
+        
+        self.view.addSubview(pageMenu.view)
     }
 
 
