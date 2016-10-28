bbb5fd5496c6ef07c910fe66312818a95535cf49
attheodo <at@atworks.gr>
2016-10-28 17:05:14 +0300

    initial workshops vc

--- DEVit/View Controllers/WorkshopsViewController.swift
+++ DEVit/View Controllers/WorkshopsViewController.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import UIKit
+
+class WorkshopsViewController: UIViewController {
+    
+    class func instantiateFromStoryboard() -> WorkshopsViewController {
+        
+        let storyboard = UIStoryboard(name: Constants.Storyboards.mainStoryboard, bundle: nil)
+        return storyboard.instantiateViewController(
+            withIdentifier: Constants.Storyboards.Scenes.mainStoryboardWorkshopsViewControllerScene) as! WorkshopsViewController
+    
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        _configureView()
+    }
+    
+    // MARK: - Private Methods
+    private func _configureView() {
+        title = "Workshops"
+    }
+
+}
