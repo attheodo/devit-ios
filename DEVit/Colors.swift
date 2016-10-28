a15e27bfbf2b6df3920f86f8291c50f700dd917b
attheodo <at@atworks.gr>
2016-10-28 14:08:31 +0300

    app wide colors as a struct

---
+++ DEVit/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  DEVit
+//
+//  Created by Athanasios Theodoridis on 27/10/2016.
+//  Copyright © 2016 devitconf. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+import SwiftHEXColors
+
+struct Colors {
+    static let darkestBlue = UIColor(hexString: "#003F74")
+    static let darkBlue = UIColor(hexString: "#00639F")
+    static let fadedWhite = UIColor(hexString: "#FFFFFF", alpha: 0.4)
+}
