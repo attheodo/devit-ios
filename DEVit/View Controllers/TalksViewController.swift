ab9769e0bb7efe427c34f79b6b123cb107757fbb
attheodo <at@atworks.gr>
2016-10-28 20:19:47 +0300

    implement talks tableview logic

--- DEVit/View Controllers/TalksViewController.swift
+++ DEVit/View Controllers/TalksViewController.swift
@@ -9,7 +9,9 @@
 import Foundation
 import UIKit
 
-class TalksViewController: UIViewController {
+import PKHUD
+
+class TalksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     class func instantiateFromStoryboard() -> TalksViewController {
         
@@ -18,16 +20,96 @@ class TalksViewController: UIViewController {
             withIdentifier: Constants.Storyboards.Scenes.mainStoryboardTalksViewControllerScene) as! TalksViewController
     
     }
-
     
+    // MARK: - IBOutlets
+    @IBOutlet weak var talksTableView: UITableView!
+
+    // MARK: - Private Properties
+    private lazy var ModelsManager = {
+       FirebaseManager.sharedInstance
+    }()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        _registerNotifications()
         _configureView()
+        _loadTalksData()
+    
     }
     
     // MARK: - Private Methods
     private func _configureView() {
         title = "Talks"
+        
         view.backgroundColor = Colors.lightBlue
+        talksTableView.backgroundColor = UIColor.clear
     }
+    
+    private func _loadTalksData() {
+
+        HUD.show(.progress)
+        
+        ModelsManager.startObservingTalkSnapshots {
+            HUD.hide()
+        }
+
+    }
+    
+    // MARK: - UITableView Datasource/Delegate
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ModelsManager.talks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let talks = ModelsManager.talks
+        
+        if talks[indexPath.row].type! == .devit {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "DevitTalkTypeCell") as! DevitTalkTypeCell
+            cell.talk = talks[indexPath.row]
+            
+            return cell
+        
+        } else {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SpeakerTalkTypeCell") as! SpeakerTalkTypeCell
+            cell.talk = talks[indexPath.row]
+            
+            return cell
+        
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        let talks = ModelsManager.talks
+        
+        if talks[indexPath.row].type! == .devit {
+            return 60.0
+        } else {
+            return 100.0
+        }
+        
+    }
+    
+    // MARK: - Notifications
+    private func _registerNotifications() {
+        
+        let nc = NotificationCenter.default
+        nc.addObserver(self, selector: #selector(self._reloadTalksTableView), name: Constants.Notifications.talksSnapshotUpdated, object: nil)
+    
+    }
+    
+    // MARK: Notification Selectors
+    @objc private func _reloadTalksTableView() {
+        talksTableView.reloadData()
+    }
+
 }
