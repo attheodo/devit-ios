de8e0944b2249216052c5e1c84f0af87145e60f1
attheodo <at@atworks.gr>
2016-10-28 17:28:56 +0300

    add model for Talk

---
+++ DEVit/Models/Talk.swift
@@ -0,0 +1,56 @@
+//
+//  Talk.swift
+//  DEVit
+//
+//  Created by Athanasios Theodoridis on 28/10/2016.
+//  Copyright © 2016 devitconf. All rights reserved.
+//
+
+import Foundation
+
+import ObjectMapper
+
+public class Talk: Mappable, CustomStringConvertible {
+    
+    /// The type of talk:
+    /// - `devit` stands for devit related sessions (i.e Coffee Break)
+    /// - `speaker` stands for talks by speakers
+    public enum TalkType: String {
+        case devit = "devit"
+        case speaker = "speaker"
+    }
+    
+    /// The order of the talk in the schedule
+    var order: Int? = 0
+    /// The name of the talk
+    var name: String? = nil
+    /// The type of the talk
+    var type: TalkType? = .devit
+    /// The tags of the talk
+    var tags: [String]? = nil
+    /// The starting date of the talk
+    var startTime: Date? = nil
+    /// The duration of the talk in minutes
+    var duration: Int? = nil
+    /// The `id` of the speaker as declared in Firebase
+    var speakerId: String? = nil
+
+    public var description: String {
+        return name!
+    }
+    
+    required public init?(map: Map) {
+        
+    }
+    
+    public func mapping(map: Map) {
+        
+        self.order <- map["order"]
+        self.name <- map["name"]
+        self.type <- (map["type"],EnumTransform<TalkType>())
+        self.tags <- map["tags"]
+        self.duration <- map["duration"]
+        self.speakerId <- map["speaker_id"]
+        
+    }
+}
