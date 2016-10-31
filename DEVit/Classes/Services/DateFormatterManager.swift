05d8719ba6f6c8a857722fe2f9a8f54c589af543
attheodo <at@atworks.gr>
2016-10-31 14:38:11 +0200

    add method to check whether current time falls within a timestamp range, add doc strings

--- DEVit/Classes/Services/DateFormatterManager.swift
+++ DEVit/Classes/Services/DateFormatterManager.swift
@@ -40,6 +40,9 @@ public class DateFormatterManager {
         
     }
     
+    /**
+        Returns a formatter capable of parsing ISO8601 dates
+    */
     public func dateFormatterForISO8601Date() -> DateFormatter {
         
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
@@ -47,6 +50,38 @@ public class DateFormatterManager {
         dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX") as Locale!
         return dateFormatter
     
+    }
+    
+    /**
+     Retruns whether the current date is withint a certain time range
+     
+     - parameter startingTime: The start of the time range 
+     - parameter duration: The duration to be added in order to mark the end of the range
+     - returns: A `Bool` indicating whether the current time is within the range
+    */
+    public func isCurrentTimeWithinTimeRange(startingTime: Date, duration: Int) -> Bool {
+        
+        // If we're on Release configuration, take into account the whole date
+        // instead of just the timestamp
+        #if !DEBUG
+            
+            let currentTime = Date().timeIntervalSince1970
+            let startTime = startingTime.timeIntervalSince1970
+            let endTime = startTime + TimeInterval((duration * 60))
+            
+        // In Debug configuration, just compare timestamps in order to test more easily
+        // whether the current time is within the range
+        #else
+            
+            let currentTime = dateWith_Hmm_formatAsString(fromDate: Date())
+            let startTime = dateWith_Hmm_formatAsString(fromDate: startingTime)
+            let timeInterval = Double(duration * 60)
+            let endTime = dateWith_Hmm_formatAsString(fromDate: startingTime.addingTimeInterval(timeInterval))
+
+        #endif
+        
+        return currentTime >= startTime && currentTime <= endTime ? true : false
+
     }
     
 }
