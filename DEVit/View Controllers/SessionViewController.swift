4cc9ed1c7a122f5ee33d3eb99e1c5228ec2d9e78
attheodo <at@atworks.gr>
2016-10-31 17:18:06 +0200

    initial SessionViewController

---
+++ DEVit/View Controllers/SessionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SessionViewController.swift
+//  DEVit
+//
+//  Created by Athanasios Theodoridis on 31/10/2016.
+//  Copyright © 2016 devitconf. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SessionViewController: UIViewController {
+    
+    // MARK: - IBActions
+    @IBAction func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
