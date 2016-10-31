1fcc433b4a40d7c5531b0badd7e3cf6b10f33f35
attheodo <at@atworks.gr>
2016-10-31 20:13:02 +0200

    add model for workshop

---
+++ DEVit/Models/Workshop.swift
@@ -0,0 +1,48 @@
+//
+//  Workshop.swift
+//  DEVit
+//
+//  Created by Athanasios Theodoridis on 31/10/2016.
+//  Copyright © 2016 devitconf. All rights reserved.
+//
+
+import Foundation
+
+import ObjectMapper
+
+public class Workshop: Mappable, CustomStringConvertible {
+    
+    /// The id of the workshop
+    var id: String? = nil
+    /// The order of the workshop in the schedule
+    var order: Int? = 0
+    /// The name of the workshop
+    var name: String? = nil
+    /// The `id` of the speaker as declared in Firebase
+    var speakerId: String? = nil
+    /// Whether the workshop takes the whole day
+    var isAllDay: Bool = false
+    /// The location where the workshop takes place
+    var location: String? = nil
+    
+    var speaker: Speaker? = nil
+    
+    public var description: String {
+        return name!
+    }
+    
+    required public init?(map: Map) {
+        
+    }
+    
+    public func mapping(map: Map) {
+        
+        self.id <- map["id"]
+        self.order <- map["order"]
+        self.name <- map["name"]
+        self.speakerId <- map["speaker_id"]
+        self.name <- map["is_all_day"]
+        self.location <- map["location"]
+        
+    }
+}
