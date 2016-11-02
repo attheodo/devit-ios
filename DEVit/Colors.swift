56ab79ee4d5653b030fa4c94e838dedcb926edc1
attheodo <at@atworks.gr>
2016-11-02 15:28:10 +0200

    add new entires, make all explicitly unwrapped optionals

--- DEVit/Colors.swift
+++ DEVit/Colors.swift
@@ -12,13 +12,17 @@ import UIKit
 import SwiftHEXColors
 
 struct Colors {
-    static let darkestBlue = UIColor(hexString: "#003F74")
-    static let darkestBlueFaded = UIColor(hexString: "#003F74", alpha: 0.15)
-    static let darkBlue = UIColor(hexString: "#00639F")
-    static let lightBlue = UIColor(hexString: "#F7FCFF")
-    static let fadedWhite = UIColor(hexString: "#FFFFFF", alpha: 0.4)
-    static let mediumBlue = UIColor(hexString: "#74ABCE")
-    static let darkGray = UIColor(hexString: "#4D4E4D")
-    static let lightGray = UIColor(hexString: "#929496")
-    static let purple = UIColor(hexString: "#A33C89")
+    static let darkestBlue = UIColor(hexString: "#003F74")!
+    static let darkestBlueFaded = UIColor(hexString: "#003F74", alpha: 0.15)!
+    static let darkBlue = UIColor(hexString: "#00639F")!
+    static let lightBlue = UIColor(hexString: "#F7FCFF")!
+    static let fadedWhite = UIColor(hexString: "#FFFFFF", alpha: 0.4)!
+    static let mediumBlue = UIColor(hexString: "#74ABCE")!
+    static let darkGray = UIColor(hexString: "#4D4E4D")!
+    static let lightGray = UIColor(hexString: "#929496")!
+    static let purple = UIColor(hexString: "#A33C89")!
+    static let yellow = UIColor(hexString: "#F3C82F")!
+    static let orange = UIColor(hexString: "#E2642E")!
+    static let green = UIColor(hexString: "#29974E")!
+    static let tagColors = [purple, yellow, orange, green]
 }
