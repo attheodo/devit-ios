d3c500b7c05f7f2f0b401b5d9c5f8473f07a862f
attheodo <at@atworks.gr>
2016-11-02 15:43:45 +0200

    configure talk tags

--- DEVit/View Controllers/SessionViewController.swift
+++ DEVit/View Controllers/SessionViewController.swift
@@ -9,7 +9,17 @@
 import Foundation
 import UIKit
 
+import TagListView
+
 class SessionViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var talkNameLabel: UILabel!
+    @IBOutlet weak var talkTagsView: TagListView!
+    
+    // MARK: - Public Properties
+    public var talk: Talk!
     
     class func instantiateFromStoryboard() -> SessionViewController {
         
@@ -30,11 +40,45 @@ class SessionViewController: UIViewController {
         
         super.viewDidLoad()
         navigationController?.isNavigationBarHidden = true
+        
+        _configureView()
     
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return UIStatusBarStyle.lightContent
+    }
+    
+    // MARK: - Private Methods
+    private func _configureView() {
+        
+        view.backgroundColor = Colors.lightBlue
+        
+        talkNameLabel.text = talk.name!
+        durationLabel.text = "\(talk.duration!) m"
+        
+        _configureTags()
+        
+    }
+    
+    private func _configureTags() {
+        
+        talkTagsView.textFont = UIFont.systemFont(ofSize: 14)
+
+        guard let tags = talk!.tags else {
+            return
+        }
+        
+        for (i, tag) in tags.enumerated() {
+            
+            let tag = talkTagsView.addTag(tag)
+            
+            tag.backgroundColor = Colors.tagColors[i % 4]
+            tag.cornerRadius = 3.0
+        
+        }
+        
+
     }
     
 }
