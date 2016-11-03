096cc48c02e5254de14733b04cd5977c2b46d428
attheodo <at@atworks.gr>
2016-11-03 16:43:57 +0200

    configure current rating

--- DEVit/TableView Cells/SessionRatingCell.swift
+++ DEVit/TableView Cells/SessionRatingCell.swift
@@ -9,10 +9,21 @@
 import Foundation
 import UIKit
 
+import Cosmos
+
 class SessionRatingCell: UITableViewCell {
     
     // MARK: - IBOutlets
     @IBOutlet weak var containerView: RoundedCornersShadowedView!
+    @IBOutlet weak var topicRatingControl: CosmosView!
+    @IBOutlet weak var presentationRatingControl: CosmosView!
+    
+    public var talk: Talk!
+    public var rating: Rating? {
+        didSet {
+            _setCurrentRating()
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,8 +33,25 @@ class SessionRatingCell: UITableViewCell {
     // MARK: - Private Methods
     private func _configureCell() {
         
+        selectionStyle = .none
         backgroundColor = UIColor.clear
         contentView.backgroundColor = UIColor.clear
         
+    }
+    
+    private func _setCurrentRating() {
+       
+        guard let rating = rating else {
+            return
+        }
+        
+        if let topicRating = rating.topic {
+            topicRatingControl.rating = topicRating
+        }
+        
+        if let presentationRating = rating.presentation {
+            presentationRatingControl.rating = presentationRating
+        }
+
     }
 }
